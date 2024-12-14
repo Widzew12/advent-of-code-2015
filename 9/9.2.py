1e75948a2aeab04f9d90980de1b288242fe0c8f6
Widzew12 <szmnglowacz@gmail.com>
2024-12-14 20:20:45 +0100

    Add day 9 files

---
+++ 9/9.2.py
@@ -0,0 +1,39 @@
+from itertools import permutations
+
+with open("9.txt", "r") as input_file:
+    input_data = [line.strip() for line in input_file.readlines()]
+    
+    distances_dict = dict()
+    destinations_set = set()
+    
+    for line in input_data:
+        split_line = line.split(" = ")
+        destination_1, destination_2 = split_line[0].split(" to ")
+        distance = split_line[1]
+        
+        if destination_1 not in distances_dict:
+            distances_dict[destination_1] = dict()
+        distances_dict[destination_1][destination_2] = int(distance)
+        
+        if destination_2 not in distances_dict:
+            distances_dict[destination_2] = dict()
+        distances_dict[destination_2][destination_1] = int(distance)
+        
+        destinations_set.add(destination_1)
+        destinations_set.add(destination_2)
+    
+    destinations_list = list(destinations_set)
+    destinations_permutations = list(permutations(destinations_list))
+    
+    max_distance = 0
+    for permutation in destinations_permutations:
+        prev_destination = None
+        distance = 0
+        for destination in permutation:
+            if prev_destination is not None:
+                distance += distances_dict[prev_destination][destination]
+            prev_destination = destination
+        
+        max_distance = max(distance, max_distance)
+        
+    print(max_distance)
